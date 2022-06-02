d3e8e1c5ede0786def7ba55a7a903e0874333998
Marco Jardim <marcoeojardim@gmail.com>
2022-06-01 22:01:46 -0300

    adding a different example

--- test/test.ts
+++ test/test.ts
@@ -35,12 +35,15 @@ describe("Verifier", async () => {
   });
 
   it("should get proof of inclusion", async function () {
-    const leaves = ['a', 'b', 'c'].map(x => ethers.utils.toUtf8Bytes(x)); // transform to bytes
+    const leaves = ['2022-06-01' + '08:00:00'+ 'dh28ej2skwio229smiwmwismsiwmwosmaowjwojajajaowk' + '34986' + 'salt(2839021jdnsljhdowowpslw02)', 'b', 'c'].map(x => ethers.utils.toUtf8Bytes(x)); // transform to bytes
     const tree = new MerkleTree(leaves, ethers.utils.keccak256, {sortPairs: true, hashLeaves: true});
     const root = tree.getHexRoot();
-    const leaf = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('a'));
+    const leaf = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('2022-06-01' + '08:00:00'+ 'dh28ej2skwio229smiwmwismsiwmwosmaowjwojajajaowk' + '34986' + 'salt(2839021jdnsljhdowowpslw02)'));
     const notLeaf = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('d'));
     const proof = tree.getHexProof(leaf);
+    console.log(root);
+    console.log(leaf);
+    console.log(proof);
   
     const setRoot = await this.verifier.setRoot(tree.getRoot());
     setRoot.wait();
